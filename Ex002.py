19dbd443e79a8c68c9e02de6a71582212bb3bd16
Magno Toninato <magno.toninato@hotmail.com>
2025-04-17 21:05:12 +0100

    Python Journey

--- Ex002.py
+++ Ex002.py
@@ -1,3 +1,7 @@
 name = input("Type your name: ")
 print("Nice to meet you {}!".format(name))
 #When use {} main formatted output.
+ 
+name = input("Type your name: ")
+print("Nice to meet you {}!".format(name))
+#When use {} main formatted output.
---
+++ Ex004.py
@@ -0,0 +1,6 @@
+n = input('TYpe something: ')
+print(n.isalnum())
+print(n.isalpha())
+print(n.islower())
+print(n.isnumeric())
+

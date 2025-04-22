9c8eb183a1d9d8812c8e3e05a2c86ae4d55ef60f
Magno Toninato <magno.toninato@hotmail.com>
2025-04-22 21:21:20 +0100

    Create Ex005.py

---
+++ Ex005.py
@@ -0,0 +1,13 @@
+n1= int(input('Type the first value: '))
+n2= int(input('Type the second value: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+DI = n1//n2
+e = n1 ** n2
+print('A soma é {}, \n o produto é {} e a \n divisão é {:.3f}'.format(s,m,d), end=' >>>>')
+# To format a long number, use the command {:.The number, put 'f'if it is a float}
+# To perform a line break, use the command "\n"
+# To leave the print commands on the same line, use the command 'end = 'sOnly double space leaves a common space, but you ca use any form'.
+print('Divisão inteira {} e potencia {}'.format(DI, e))
+

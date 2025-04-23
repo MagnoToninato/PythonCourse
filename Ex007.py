68f3f460f7633905961a603a3aea9ef4b674bbcd
Magno Toninato <magno.toninato@hotmail.com>
2025-04-23 21:37:14 +0100

    Number Show double, triple and Roof

---
+++ Ex007.py
@@ -0,0 +1,7 @@
+N = int(input('Type a number: '))
+D = N*2
+T = N*3
+R = N**(1/2)
+
+print('The double of {} value {}.'.format(N, D))
+print('The triple of {} value {}. \nThe roof of {} value {:.2f}.'.format(N, T, N, R))

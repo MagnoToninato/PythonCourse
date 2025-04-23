90b6dad4625a18cf2e56b8598a168bc64217966c
Magno Toninato <magno.toninato@hotmail.com>
2025-04-23 21:45:14 +0100

    Using the print function

--- Ex002.py
+++ Ex002.py
@@ -1,7 +1,4 @@
 name = input("Type your name: ")
 print("Nice to meet you {}!".format(name))
 #When use {} main formatted output.
- 
-name = input("Type your name: ")
-print("Nice to meet you {}!".format(name))
-#When use {} main formatted output.
+

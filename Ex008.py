0c6fba46ac4c60612ac6a8b4b6eb4f3ad1bf9fa6
Magno Toninato <magno.toninato@hotmail.com>
2025-04-23 21:55:43 +0100

    Arithmetic mean

---
+++ Ex008.py
@@ -0,0 +1,4 @@
+N1 = float(input('Students first grade: '))
+N2 = float(input('Students second grade: '))
+Average = (N1 + N2) / 2
+print('The average between {} e {} and is equal to {}'.format(N1, N2, Average))

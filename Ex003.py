7f2665bb9ae85e46524ed5210079d327f069becf
Magno Toninato <magno.toninato@hotmail.com>
2025-04-23 21:43:50 +0100

    Adding two numbers

--- Ex003.py
+++ Ex003.py
@@ -2,5 +2,5 @@ n1 = int(input('Type the first number:'))
 n2 = int(input('Type the second number:'))
 s = n1+n2
 # print('The sum between', n1, 'and', n2, 'is equal to', s)
-# Formatação antiga, ainda utilizada porém iremos usar a nova para melhores praticas.
+# Formatação antiga, ainda utilizada porém iremos usar a nova para melhores praticas
 print('The sum between {} and {} equal to {}'.format(n1,n2,s))

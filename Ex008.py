a6d28a259b1ce785c95b83b36be9d627b6d35a3c
Magno Toninato <magno.toninato@hotmail.com>
2025-04-23 21:58:01 +0100

    Update Ex008.py

--- Ex008.py
+++ Ex008.py
@@ -1,4 +1,4 @@
 N1 = float(input('Students first grade: '))
 N2 = float(input('Students second grade: '))
 Average = (N1 + N2) / 2
-print('The average between {} e {} and is equal to {}'.format(N1, N2, Average))
+print('The average between {:.1f} e {:.1f} and is equal to {:.1f}'.format(N1, N2, Average))

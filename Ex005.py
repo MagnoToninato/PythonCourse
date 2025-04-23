75ddaabd9853f8d80cbd8b034485a92654aae539
Magno Toninato <magno.toninato@hotmail.com>
2025-04-23 21:41:05 +0100

    Arithmetics Operations

--- Ex005.py
+++ Ex005.py
@@ -8,6 +8,6 @@ P = n1 ** n2
 print('The sum is {}, the product is {} and the division is {:.3f}'.format(s,m,d), end=' >>>> ')
 # To format a long number, use the command {:.The number, put 'f'if it is a float}
 # To perform a line break, use the command "\n"
-# To leave the print commands on the same line, use the command 'end = 'sOnly double space leaves a common space, but you ca use any form'.
+# To leave the print commands on the same line, use the command 'end = 'sOnly double space leaves a common space, but you ca use any form'
 print('Integer division is {} and power is {}'.format(Id, P))
 

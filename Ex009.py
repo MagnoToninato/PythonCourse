a37ece44fe96bfa8afc6f9b0cddc40e6f4c2c1eb
Magno Toninato <magno.toninato@hotmail.com>
2025-04-24 22:41:42 +0100

    Measurement Converter

--- Ex009.py
+++ Ex009.py
@@ -2,7 +2,7 @@ measures = float(input('Write a value for distance in meters: '))
 km = measures / 1000
 hm = measures / 100
 dam = measures /10
-dm =measures * 10
+dm = measures * 10
 cm = measures * 100
 mm = measures * 1000
 print('The measurement of {}m corresponds to \n{}km \n{}hm \n{}dam \n{:.0f}dm \n{:.0f}cm \n{:.0f}mm'.format(measures, km, hm, dam, dm, cm, mm))
